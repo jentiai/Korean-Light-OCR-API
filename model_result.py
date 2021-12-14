e3f385ae5fa92fabef009dd658818ec63fc7b681
shindonghyun <donghyun_shin@jenti.ai>
2021-12-14 10:45:43 +0000

    [feat]:Add model result

--- model_result.py
+++ model_result.py
@@ -5,28 +5,33 @@ import argparse
 from tools import visualization
 
 
-def main():
+def main(args):
+    img_dir = args.img_dir
+    output_dir = args.output_dir
+    url = args.url
 
-    output_dir = "./output"
+
     if not os.path.isdir(output_dir):
         os.makedirs(output_dir, exist_ok = True)
 
-    url = "http://27.255.77.102:5000/evaluation"
-    img_path = './1.jpg'
-    files = {'file': open(img_path, 'rb').read()}
-    r = requests.post(url, files = files)
-
-    visualization(img_path,  r.json(), output_dir)
+    
+    for img_name in list(filter(lambda x: x.find('.jpg') != -1 or x.find('.png') != -1, os.listdir(img_dir))):
+        img_path = os.path.join(img_dir, img_name)
+        files = {'file': open(img_path, 'rb').read()}
+        r = requests.post(url, files = files)
+        visualization(img_path,  r.json(), output_dir)
 
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--api_res_json', type = str,
-                        help = 'path to pred_all.json')
-    parser.add_argument('--txt_gt_dir', type = str,
-                        help = 'path to gt dir')
+    parser.add_argument('--img_dir', type = str,
+                        help = 'input image directory')
+    parser.add_argument('--output_dir', type = str,
+                        help = 'output image directory')
+    parser.add_argument('--url', type = str, default="http://27.255.77.102:5000/evaluation", help = 'output image directory')
+
     args = parser.parse_args()
 
-    Reader = ReadPredGT(args)    
-    pred_json = Reader.read_pred_json()
+    main(args)    
+    
